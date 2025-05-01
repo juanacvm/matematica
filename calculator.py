9db4a6043d597e4746fa1aae090fe6cb1242b670
Juan <juan_acv99@hotmail.com>
2025-05-01 11:15:20 -0500

    add multiply and division functions

--- calculator.py
+++ calculator.py
@@ -3,3 +3,11 @@ def addNumbers(num1, num2):
 
 def subtractNumbers(num1, num2):
     return num1 - num2
+
+def multiplyNumbers(num1, num2):
+    return num1 * num2
+
+def divideNumbers(num1, num2):
+    if num2 == 0:
+        raise "Error: Division by zero is not allowed."
+    return num1 / num2
